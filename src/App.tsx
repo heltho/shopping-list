1c85292bd0de48d8f9824c3a6bca6495bd4f8f6e
Thomas Hellin <thomas.hellin@hotmail.com>
2022-12-30 23:03:17 +0100

    chore: add tailwind new colors + clear index html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h2 className="text-6xl font-bold text-orange-500">Hello World</h2>;
+  return <h2 className="text-6xl font-bold text-mandy-500">Hello World</h2>;
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mandy: {
+          DEFAULT: "#EA446A",
+          50: "#FDE9EE",
+          100: "#FAD7DF",
+          200: "#F6B2C2",
+          300: "#F28DA4",
+          400: "#EE6987",
+          500: "#EA446A",
+          600: "#DD1946",
+          700: "#AB1336",
+          800: "#780D26",
+          900: "#460816",
+        },
+      },
+    },
   },
   plugins: [],
 };
